85a70a273a1135607261dc28a72f62972706bfb0
lucaswyd <99453165+lucaswyd@users.noreply.github.com>
2024-07-09 01:22:36 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,68 +1,55 @@
-import "@/styles/globals.scss";
-import Layout from "@/components/Layout";
+// _app.tsx
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
 import Head from "next/head";
 import { Toaster } from "sonner";
-import "@/styles/checkbox.scss";
-import "react-tooltip/dist/react-tooltip.css";
-import { Tooltip } from "react-tooltip";
-import Router from "next/router";
-import { useState, useEffect } from "react";
-import NProgress from "nprogress";
-import "@/styles/nprogress.scss";
-import "react-loading-skeleton/dist/skeleton.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import Layout from "@/components/Layout";
+import "../styles/globals.scss"; // Ensure your global styles are imported
 
-export default function App({ Component, pageProps }: any) {
-  const [isLoading, setIsLoading] = useState(false);
-  NProgress.configure({ showSpinner: false });
-  // NProgress.configure({
-  //   template: '<div class="bar" role="bar"><div class="peg"></div></div>'
-  // });
+import type { AppProps } from "next/app";
+import type { NextRouter } from "next/router";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const router: NextRouter = useRouter();
+
   useEffect(() => {
-    Router.events.on("routeChangeStart", (url) => {
-      setIsLoading(true);
+    const script = document.createElement("script");
+    script.src = "/snapping.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
       NProgress.start();
-    });
+    };
 
-    Router.events.on("routeChangeComplete", (url) => {
-      setIsLoading(false);
-      NProgress.done(false);
-    });
+    const handleRouteChangeComplete = () => {
+      NProgress.done();
+    };
 
-    Router.events.on("routeChangeError", (url) => {
-      setIsLoading(false);
-    });
-  }, [Router]);
+    router.events.on("routeChangeStart", handleRouteChangeStart);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+    };
+  }, [router]);
+
+  const GTag: any = process.env.NEXT_PUBLIC_GT_MEASUREMENT_ID;
+
   return (
     <>
       <Head>
-        <title>Cinemaos</title>
-        <meta name="description" content="Your Personal Streaming Hub" />
-        <meta
-          name="keywords"
-          content="movie, streaming, tv, rive, stream. movie app, tv shows, movie download"
-        />
-        <meta
-          name="google-site-verification"
-          content="J0QUeScQSxufPJqGTaszgnI35U2jN98vVWSOkVR4HrI"
-        />
-        <link rel="manifest" href="manifest.json" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#f4f7fe" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Rive" />
-        <link rel="icon" href="/images/logo512.png" />
-        <link rel="apple-touch-icon" href="/images/logo512.png" />
-        {/* <link rel="mask-icon" href="/images/logo512.svg" color="#f4f7fe" /> */}
-        <meta name="mobile-web-app-capable" content="yes" />
-        {/* <meta name="msapplication-TileColor" content="#f4f7fe" /> */}
-        <meta name="msapplication-tap-highlight" content="no" />
-        <link rel="shortcut icon" href="/images/logo512.png" />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3701741788853666"
-          crossOrigin="anonymous"
-        />
+        <title>Rive</title>
+        {/* Add your meta tags, links, and other head elements */}
       </Head>
       <Layout>
         <Toaster
@@ -77,9 +64,11 @@ export default function App({ Component, pageProps }: any) {
           }}
           position="top-center"
         />
-        <Tooltip id="tooltip" className="react-tooltip" />
         <Component {...pageProps} />
       </Layout>
+      <GoogleAnalytics gaId={GTag} />
     </>
   );
 }
+
+export default MyApp;
